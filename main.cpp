6a7deb17c387d76942690694cd5fe8c8e4680726
253884 <73070465+253884@users.noreply.github.com>
2020-10-31 21:32:36 +0100

    compiles

--- main.cpp
+++ main.cpp
@@ -4,21 +4,28 @@
 #define X 0
 #define Y 1
 
-using std::cin, std::cout, std::endl, std::vector;
+using std::cin;
+using std::cout;
+using std::endl;
+using std::vector;
 
 unsigned int howManyObjects;
-vector<vector<double> > sourceDimensions[2];
+vector<double> sourceDimensions[2];
 
 
 int main(){
-	cout << "Wprowadź ilosc obiektow:" << endl;
-	while( cin >> howManyObjects < 0 )
-		cout << "Wprowadzona liczba jest mniejsza niz zero.\nWprowadz poprawna ilosc obiektow:" << endl;
+	cout << "Wprowadz ilosc obiektow:" << endl;
+	while( cin >> howManyObjects ){
+		if( howManyObjects > 0 )
+			break;
+		else
+			cout << "Wprowadzono nieprawidlowa ilosc obiektow.\nWprowadz ilosc obiektow:" << endl;
+	}
 	
-	for( int i = 0; i < howManyObjects; ++i ){
+	for( unsigned int i = 0; i < howManyObjects; ++i ){
 		double tempX, tempY;
 		
-		cout << "Wprowadź wymiary obiektu ( x, y ):" << endl;
+		cout << "Wprowadz wymiary obiektu ( x, y ):" << endl;
 		cin >> tempX >> tempY;
 		
 		sourceDimensions[X].push_back( tempX );
